bd08e1e7b44082c71258b842bf5d55fc489c2522
Rashaad Ahmed Khan <rashaadakhan@gmail.com>
2021-05-31 21:46:37 +0530

    displays the words

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react'
+import './App.css'
+import Header from './components/Header'
+import Figure from './components/Figure'
+import WrongLetters from './components/WrongLetters'
+import Word from './components/Word'
+
+const words = ['programming', 'coding', 'github', 'stackoverflow']
+let selectedWord = words[Math.floor(Math.random()*words.length)]
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const[playable, setPlayable] = useState(true)
+    const[correctLetters, setCorrectLetters] = useState([])
+    const[wrongLetters, setWrongLetters] = useState([])
+    const[showNotification, setShowNotification] = useState(false)
+
+    return (
+        <>
+            <Header />
+            <div className = "game-container">
+                <Figure wrongLetters = {wrongLetters} />
+                <WrongLetters wrongLetters={wrongLetters} />
+                <Word selectedWord={selectedWord} correctLetters={correctLetters} />
+            </div>
+        </>
+    );
 }
 
-export default App;
+export default App
---
+++ src/components/Notification.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Notification = () => {
+    return (
+        <div class = "notification-container" id = "notification-container">
+            <p>This letter has already been used.</p>
+        </div>
+    )
+}
+
+export default Notification
