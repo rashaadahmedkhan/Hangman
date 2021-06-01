69e8fce8f03b91b1e1350f66d4b74b1ff5c7889e
Rashaad Ahmed Khan <rashaadakhan@gmail.com>
2021-06-01 21:22:31 +0530

    update wrong letters

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Header from './components/Header'
 import Figure from './components/Figure'
 import WrongLetters from './components/WrongLetters'
 import Word from './components/Word'
+import Popup from './components/Popup'
+import Notification from './components/Notification'
 
 const words = ['programming', 'coding', 'github', 'stackoverflow']
 let selectedWord = words[Math.floor(Math.random()*words.length)]
@@ -14,6 +16,33 @@ function App() {
     const[correctLetters, setCorrectLetters] = useState([])
     const[wrongLetters, setWrongLetters] = useState([])
     const[showNotification, setShowNotification] = useState(false)
+
+    useEffect(() => {
+        const handleKeydown = event => {
+            const {key, keyCode} = event
+            if(playable && keyCode >= 65 && keyCode <= 90) {
+                const letter = key.toLowerCase()
+                if(selectedWord.includes(letter)) {
+                    if(!correctLetters.includes(letter)) {
+                        setCorrectletters(correctLetters => [...currentLetters, letter])
+                    }
+                    else {
+                        show(setShowNotification)
+                    }
+                }
+                else {
+                    if(!wrongLetters.includes(letter)) {
+                        setWrongLetters(currentLetters => [...currentLetters, letter])
+                    }
+                    else {
+                        show(setShowNotification)
+                    }
+                }
+            }
+        }
+        window.addEventListener('keydown', handleKeydown)
+        return () => window.removeEventListener('keydown', handleKeydown)
+    }, [correctLetters, wrongLetters, playable])
 
     return (
         <>
